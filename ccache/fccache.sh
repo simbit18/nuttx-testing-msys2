b8072a37e302cb9beb79057abce9623135b555b4
simbit18 <101105604+simbit18@users.noreply.github.com>
2024-03-12 17:52:39 +0100

    Create fccache.sh

---
+++ ccache/fccache.sh
@@ -0,0 +1,67 @@
+#!/bin/sh
+
+set -e
+set -o xtrace
+
+WD=$(cd "$(dirname "$0")" && pwd)
+WORKSPACE=$(cd "${WD}"/../ && pwd -P)
+
+tools=${WORKSPACE}/tools
+EXTRA_PATH=
+
+add_path() {
+  PATH=$1:${PATH}
+  EXTRA_PATH=$1:${EXTRA_PATH}
+}
+
+check_cmd() {
+    command -v "$1" > /dev/null 2>&1
+}
+
+ccache_test() {
+  add_path "${tools}"/ccache/bin
+  # Configuring the PATH environment variable
+  # export CARGO_HOME=${tools}/rust/cargo
+  export CCACHE_DIR=${tools}/ccache/bin
+  if ! type ccache > /dev/null 2>&1; then
+    pacman -S --noconfirm --needed ccache
+  fi
+  ccache -s
+  setup_links
+  gcc ${WD}/exception_setjmp.c -o exception_setjmp
+  ccache -s
+  ccache -p
+  command ccache --version
+  
+}
+
+setup_links() {
+  # Configure ccache Fedora
+  mkdir -p "${tools}"/ccache/bin/
+  # ok local
+  # export MSYS=winsymlinks:lnk
+  # export MSYS=winsymlinks:nativestrict
+  # MSYS=winsymlinks:nativestrict ln -sf "$(which ccache)" "${tools}"/ccache/bin/cc
+  # MSYS=winsymlinks:nativestrict ln -sf "$(which ccache)" "${tools}"/ccache/bin/c++
+  # MSYS=winsymlinks:nativestrict ln -sf "$(which ccache)" "${tools}"/ccache/bin/gcc
+  ln -sf "$(which ccache)" "${tools}"/ccache/bin/g++
+  ln -sf "$(which ccache)" "${tools}"/ccache/bin/cc
+  ln -sf "$(which ccache)" "${tools}"/ccache/bin/c++
+  ln -sf "$(which ccache)" "${tools}"/ccache/bin/gcc
+  # cp -a "$(which ccache)" "${tools}"/ccache/bin/g++
+  # cp -a "$(which ccache)" "${tools}"/ccache/bin/cc
+  # cp -a "$(which ccache)" "${tools}"/ccache/bin/c++
+  # cp -a "$(which ccache)" "${tools}"/ccache/bin/gcc
+  ls -l "${tools}"/ccache/bin
+}
+
+
+
+
+main() {
+  mkdir -p "${tools}"
+  cd "${tools}"
+  ccache_test
+
+}
+main
